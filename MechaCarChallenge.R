55a377a64224fcf655956280b4dd248f21ad8746
MCRobish <marycate529@gmail.com>
2022-09-26 18:39:04 -0400

    Deliverable 2: creation of variance tables for suspension PSI

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,7 @@
 
 ## Dependencies 
 library(dplyr)
+library(tidyverse)
 
 ##Read in the mpg file
 mpg <- read.csv(file="Resources/MechaCar_mpg.csv")
@@ -11,3 +12,17 @@ mpg <- read.csv(file="Resources/MechaCar_mpg.csv")
 mpg_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data = mpg)
 summary(mpg_lm)
 
+## import suspension data for deliverable 2
+suspension <- read.csv(file="Resources/Suspension_Coil.csv")
+##suspension
+
+##create total summary
+
+total_summary <- suspension %>%
+  summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),Std_Dev=sd(PSI))
+
+##create lot summary dataframe
+
+lot_summary <- suspension %>%
+  group_by(Manufacturing_Lot) %>% 
+  summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),Std_Dev=sd(PSI))
